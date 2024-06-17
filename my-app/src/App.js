d6033cfdb2162b364a206fb76815a76f680c57d7
Om Unadkat <omunadkat2@gmail.com>
2024-06-17 16:36:42 +0530

    react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,21 @@
 import logo from './logo.svg';
-
 import { useState } from 'react';
+import navbar from './navbar';
+import React from 'react';
+
 
 function App()
 {
-  const [value, setvalue] = useState(0)
+  const [value, setValue] = useState(0)
 
   return
   (
 <div className = "app">
+  <navbar/>
 <div className='value'>{value}</div>
-<button onClick={() => {setvalue(value +1)}}> click me</button>
+<button onClick={() => {setValue(value +1)}}> click me</button>
  
 </div>
   );
-    
-
-  
 }
 export default App ;
---
+++ my-app/src/navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+      </ul>
+    </div>
+  )
+}
+
+export default navbar
